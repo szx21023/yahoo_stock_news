82031295a0acea044db3e273a150b90d01664e5e
Herry <szx21023@gmail.com>
2023-06-16 10:42:49 +0800

    beautifulsoup parser text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 
 # 創建一個Chrome瀏覽器實例
 driver = webdriver.Chrome()
@@ -11,16 +12,21 @@ driver.get("https://tw.stock.yahoo.com/quote/2330.TW")
 # 等待搜尋結果頁面載入完成
 driver.implicitly_wait(10)
 
-# 找到所有的搜索結果元素
-element = driver.find_element(By.ID, "YDC-Stream")
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+element = soup.find('div', {
+    'id': 'YDC-Stream'})
+elements = element.find_all('li', {'class': 'js-stream-content Pos(r)'})
+for element in elements:
+    # filter ad
+    if element.find('div', {'class': 'controller gemini-ad native-ad-item Feedback Pos(r)'}):
+        continue
 
-link_elements = element.find_elements(By.TAG_NAME, "a")
-# 提取並打印連結的URL
-for link_element in link_elements:
-    parent_element = link_element.find_element(By.XPATH, "..")
-    print(parent_element.text)
-    url = link_element.get_attribute("href")
-    print(url)
+    print(element.getText())
+    urls = element.find_all("a")
+    for url in urls:
+        print(url.get("href"))
+
+    print('----------------------------')
 
 # 關閉瀏覽器
 driver.quit()
