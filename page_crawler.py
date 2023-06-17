dd499ef202701668ec0b9a73c8932754d1e86675
Herry <szx21023@gmail.com>
2023-06-17 09:10:24 +0800

    add page_crawler.py

---
+++ page_crawler.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'https://tw.stock.yahoo.com/news/%E5%8F%B0%E7%A9%8D%E9%9B%BB%E5%8F%97%E6%83%A0%E5%85%88%E9%80%B2%E8%A3%BD%E7%A8%8B%E6%BC%B2%E5%83%B9-%E7%A0%94%E8%AA%BF%E4%BC%B0%E6%98%8E%E5%B9%B4%E7%87%9F%E6%94%B6%E5%8F%AF%E6%9C%9B%E8%B7%B3%E5%A2%9E22-5-092545480.html'
+info = requests.get(url)
+soup = bs(info.text,"lxml")
+
+article = soup.find('article', {'role': 'article'})
+title = article.find('h1', {'data-test-locator': 'headline'})
+author = article.find('span', {'class': 'caas-author-byline-collapse'})
+post_time = article.find('time', {'class': 'caas-attr-meta-time'}).get('datetime')
+post_content = article.find('div', {'class': 'caas-body'})
+print(title.text)
+print(author.text)
+print(post_time)
+print(post_content.text)
