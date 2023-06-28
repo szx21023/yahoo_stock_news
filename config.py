1bfe059c7a21a6137c28a8d1bbff984047459550
Herry <szx21023@gmail.com>
2023-06-28 21:37:36 +0800

    feat: create gcp sql connection

--- config.py
+++ config.py
@@ -7,3 +7,12 @@ db_port = 3306
 db_user = ''
 db_password = ''
 db_database = ''
+
+gcp_db_project = ''
+gcp_db_region = ''
+gcp_db_instance = ''
+gcp_db_user = ''
+gcp_db_password = ''
+gcp_db_database = ''
+
+GOOGLE_APPLICATION_CREDENTIALS = ''
