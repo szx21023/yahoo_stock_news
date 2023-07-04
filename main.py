c609470affab681d92b7d864b79bab6b2834c760
Herry <szx21023@gmail.com>
2023-07-04 16:43:01 +0800

    refactor: add a class crawler, reduce useless code

--- main.py
+++ main.py
@@ -1,57 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-
 from datetime import datetime
 
 from config import host_url, stock_code, region
-from page_crawler import parser_content
+from page_crawler import yahoo_crawler
 from connect_db import stock_news
 
-options = Options()
-options.add_argument('--headless')
+# l = yahoo_crawler.parser_stock(host_url, stock_code, region)
 
-# 創建一個Chrome瀏覽器實例
-driver = webdriver.Chrome(options=options)
+# for news in l:
+#     crawl_time = str(datetime.now())
+#     stock_news.insert(news['title'], news['author'], news['post_time'], news['post_content'], news['url'], news['source'], crawl_time)
+#     print('----------------------------')
 
-# 打開Google首頁
-driver.get(f"{host_url}/{stock_code}.{region}")
-
-# 等待搜尋結果頁面載入完成
-driver.implicitly_wait(10)
-
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-# element = soup.find('div', {
-#     'id': 'YDC-Stream'})
-elements = soup.find_all('li', {'class': 'js-stream-content Pos(r)'})
-for element in elements:
-    # filter ad
-    if element.find('div', {'class': 'controller gemini-ad native-ad-item Feedback Pos(r)'}):
-        continue
-
-    # source web, post_time
-    tags = element.find_all("span")
-    source = ''.join([tag.text for tag in tags[:1]])
-
-    # title
-    tags = element.find_all("h3")
-    title = ''.join([tag.text for tag in tags])
-
-    # # outline
-    # tags = element.find_all("p")
-    # for tag in tags:
-    #     print(tag)
-
-    # link_url
-    tags = element.find_all("a")
-    url = tags[0].get("href")
-    attr = parser_content(url)
-
-    crawl_time = str(datetime.now())
-    stock_news.insert(attr['title'], attr['author'], attr['post_time'], attr['post_content'], url, source, crawl_time)
-    print('----------------------------')
-
-# 關閉瀏覽器
-driver.quit()
+# # 關閉瀏覽器
+# yahoo_crawler.driver.quit()
+stock_news.select()
--- page_crawler.py
+++ page_crawler.py
@@ -1,19 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 import requests
-from bs4 import BeautifulSoup as bs
 
-def parser_content(url):
-    info = requests.get(url)
-    soup = bs(info.text,"lxml")
+class YahooCrawler:
+    def __init__(self):
+        options = Options()
+        options.add_argument('--headless')
+        # 創建一個Chrome瀏覽器實例
+        self.driver = webdriver.Chrome(options=options)
 
-    article = soup.find('article', {'role': 'article'})
-    title = article.find('h1', {'data-test-locator': 'headline'})
-    author = article.find('span', {'class': 'caas-author-byline-collapse'})
-    post_time = article.find('time', {'class': 'caas-attr-meta-time'}).get('datetime')
-    post_content = article.find('div', {'class': 'caas-body'})
-    return {
-        'url': url,
-        'title': title.text,
-        'author': author.text,
-        'post_time': post_time,
-        'post_content': post_content.text
-    }
+    def parser_stock(self, host_url, stock_code, region):
+        # 打開Google首頁
+        self.driver.get(f"{host_url}/{stock_code}.{region}")
+
+        # 等待搜尋結果頁面載入完成
+        self.driver.implicitly_wait(10)
+
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        elements = soup.find_all('li', {'class': 'js-stream-content Pos(r)'})
+        l = []
+        for element in elements:
+            # filter ad
+            if element.find('div', {'class': 'controller gemini-ad native-ad-item Feedback Pos(r)'}):
+                continue
+
+            # source web, post_time
+            tags = element.find_all("span")
+            source = ''.join([tag.text for tag in tags[:1]])
+
+            # # title
+            # tags = element.find_all("h3")
+            # title = ''.join([tag.text for tag in tags])
+
+            # # outline
+            # tags = element.find_all("p")
+            # for tag in tags:
+            #     print(tag)
+
+            # link_url
+            tags = element.find_all("a")
+            url = tags[0].get("href")
+
+            attr = self.parser_content(url)
+            attr['source'] = source
+            l.append(attr)
+        return l
+
+    @staticmethod
+    def parser_content(url):
+        info = requests.get(url)
+        soup = BeautifulSoup(info.text,"lxml")
+
+        article = soup.find('article', {'role': 'article'})
+        title = article.find('h1', {'data-test-locator': 'headline'})
+        author = article.find('span', {'class': 'caas-author-byline-collapse'})
+        post_time = article.find('time', {'class': 'caas-attr-meta-time'}).get('datetime')
+        post_content = article.find('div', {'class': 'caas-body'})
+        return {
+            'url': url,
+            'title': title.text,
+            'author': author.text,
+            'post_time': post_time,
+            'post_content': post_content.text
+        }
+
+yahoo_crawler = YahooCrawler()
