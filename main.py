6e5a236593fdbbc44af5ad1aa1f1b7ea406a6992
Herry <szx21023@gmail.com>
2023-07-10 10:53:38 +0800

    refactor: change input params

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from connect_db import stock_news
 from utils import strip_colon
 
 sql_query = "SELECT title FROM news"
-title_l = set(row[0] for row in stock_news.select(sql_query))
+existed_title_l = set(row[0] for row in stock_news.select(sql_query))
 for stock_code in stock_list:
     news_l = yahoo_crawler.parser_stock(host_url, stock_code, region)
 
     for news in news_l:
         crawl_time = str(datetime.now())
-        if news['title'] in title_l:
+        if news['title'] in existed_title_l:
             continue
-        stock_news.insert(stock_code, news['title'], news['author'], news['post_time'], strip_colon(news['post_content']), news['url'], news['source'], crawl_time)
+        news['stock_code'] = stock_code
+        news['post_content'] = strip_colon(news['post_content'])
+        news['crawl_time'] = crawl_time
+        stock_news.insert(**news)
         print('----------------------------')
 
 # 關閉瀏覽器
